9b9e5310da836977e1549ae526dc78c1bd01e751
df@ <@>
2021-09-12 18:12:26 +0100

    Started data structs.

--- lost_cities_score/src/main.rs
+++ lost_cities_score/src/main.rs
@@ -1,3 +1,28 @@
+/*
+round 1:
+    player 1: ddd23456789t dt
+    player 2: d2 d3 d4 678 t
+round 2:
+    player 1: ddd23456789t dt
+    player 2: d2 d3 d4 678 t
+round 3:
+    player 1: ddd23456789t dt
+    player 2: d2 d3 d4 678 t
+*/
+
+enum Card {
+    double,
+    number(u8),
+    ten,
+}
+
+struct ExpeditionCards {
+    cards: Vec<Card>,
+}
+
+struct RoundExpeditions {
+    expeditions: Vec<ExpeditionCards>,
+}
+
 fn main() {
-    println!("Hello, world!");
 }
