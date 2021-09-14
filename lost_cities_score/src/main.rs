79a3e30660b1393e5c2c74c951f7e33412bbbcda
df@ <@>
2021-09-14 20:41:26 +0100

    clean up.

--- lost_cities_score/src/main.rs
+++ lost_cities_score/src/main.rs
@@ -1,23 +1,10 @@
-/*
-game:
-    round 1:
-        player 1: "ddd23456789t dt"
-        player 2: "d2 d3 d4 678 t"
-    round 2:
-        player 1: "ddd23456789t dt"
-        player 2: "d2 d3 d4 678 t"
-    round 3:
-        player 1: "ddd23456789t dt"
-        player 2: "d2 d3 d4 678 t"
-*/
-
-// use std::io;
 use chrono::{DateTime, Utc};
 use std::io::{self, Write};
 
 struct Player {
     score: i16,
 }
+
 #[derive(Debug)]
 enum Error {
     CardError(char),
