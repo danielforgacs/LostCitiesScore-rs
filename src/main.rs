f8f943774e960505ee1cb12f79c9c6bf7ea6feab
df@ <@>
2023-04-09 16:47:30 +0100

    cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ struct LoggedResult {
 
 fn main() {
     let logname = create_game_log_name();
-
     println!(
         "===== Lost Cities Scores Counter =====\n\
         type 'quit' to quit the game. \
@@ -40,8 +39,11 @@ fn main() {
     let mut players: [Player; 2] = [Player::new(), Player::new()];
 
     for round in 0..=2 {
-        let logline = format!("=================================================\n\
-            >>>>>>>>>>> ROUND: {} <<<<<<<<<<<\n", round+1);
+        let logline = format!(
+            "=================================================\n\
+            >>>>>>>>>>> ROUND: {} <<<<<<<<<<<\n",
+            round + 1
+        );
         print!("{}", logline);
         log += logline.as_str();
         let mut round_scores: Vec<i16> = Vec::new();
@@ -81,20 +83,28 @@ fn main() {
                         log += logline.as_str();
                         log += result.logtext.as_str();
                         round_breakdowns.push(result.logtext);
-                        break score
-                    },
-                    Err(Error::CardError(card)) => { println!("Bad card: \"{:?}\"!", card); },
+                        break score;
+                    }
+                    Err(Error::CardError(card)) => {
+                        println!("Bad card: \"{:?}\"!", card);
+                    }
                 };
             };
 
             player.score += round_score;
             round_scores.push(round_score);
-
-
         }
         let mut logtext = "_________________________________________________".to_string();
-        logtext += format!("\nplayer 1 score: {} - total: {}", round_scores[0], players[0].score).as_str();
-        logtext += format!("\nplayer 2 score: {} - total: {}\n", round_scores[1], players[1].score).as_str();
+        logtext += format!(
+            "\nplayer 1 score: {} - total: {}",
+            round_scores[0], players[0].score
+        )
+        .as_str();
+        logtext += format!(
+            "\nplayer 2 score: {} - total: {}\n",
+            round_scores[1], players[1].score
+        )
+        .as_str();
         print!("{}", logtext);
         log += logtext.as_str();
     }
@@ -123,14 +133,20 @@ fn main() {
 
     match std::fs::write(logname, log) {
         Ok(_) => {}
-        Err(_) => { println!("Could not save log file sof some reason."); }
+        Err(_) => {
+            println!("Could not save log file sof some reason.");
+        }
     };
 }
 
 fn create_game_log_name() -> String {
     loop {
         let now: DateTime<Utc> = Utc::now();
-        let logname = format!("{}_{}.txt", GAME_LOG_FILE_NAME, now.format(GAME_LOG_DATE_TEMPLATE));
+        let logname = format!(
+            "{}_{}.txt",
+            GAME_LOG_FILE_NAME,
+            now.format(GAME_LOG_DATE_TEMPLATE)
+        );
 
         if !std::path::Path::new(&logname).exists() {
             return logname;
@@ -146,35 +162,39 @@ fn sanity_check_player_cards(user_input: &str) -> bool {
         "\n" | "" => {
             println!("User must have cards!");
             return false;
-        },
-        _ => {},
+        }
+        _ => {}
     };
 
     for expedition in user_input.split(' ') {
         expedition_count += 1;
 
         if expedition_count > 6 {
-            println!("Too many expeditions. Count: {}, Cards: {}", expedition_count, user_input);
+            println!(
+                "Too many expeditions. Count: {}, Cards: {}",
+                expedition_count, user_input
+            );
             is_input_valid = false;
-            break
+            break;
         };
 
-        let mut valid_cards = vec!['d', 'd', 'd', '2', '3', '4', '5', '6', '7', '8', '9', 't', '\n'];
+        let mut valid_cards = vec![
+            'd', 'd', 'd', '2', '3', '4', '5', '6', '7', '8', '9', 't', '\n',
+        ];
 
         'card_loop: for card in expedition.chars() {
-
             for index in 0..valid_cards.len() {
                 if card == valid_cards[index] {
                     valid_cards.remove(index);
                     continue 'card_loop;
                 };
-            };
+            }
 
             println!("Bad or duplicate card: \"{}\"", card);
             is_input_valid = false;
             break;
-        };
-    };
+        }
+    }
 
     is_input_valid
 }
@@ -191,12 +211,17 @@ fn calc_player_round_score(line: &str) -> Result<LoggedResult, Error> {
                 round_score += score;
                 let logline = format!("            expedition: {}, score: {}\n", colour, score);
                 logtext += logline.as_str();
-            },
-            Err(Error::CardError(card)) => { return Result::Err(Error::CardError(card)); }
+            }
+            Err(Error::CardError(card)) => {
+                return Result::Err(Error::CardError(card));
+            }
         };
     }
 
-    Result::Ok(LoggedResult { result: round_score, logtext })
+    Result::Ok(LoggedResult {
+        result: round_score,
+        logtext,
+    })
 }
 
 fn calc_expedition_score(cards_text: &String) -> Result<i16, Error> {
@@ -206,7 +231,9 @@ fn calc_expedition_score(cards_text: &String) -> Result<i16, Error> {
     for card in cards_text.trim().chars() {
         match card {
             'd' => doubler += 1,
-            '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => { score += card.to_digit(10).unwrap() as i16 }
+            '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                score += card.to_digit(10).unwrap() as i16
+            }
             't' | '1' => score += 10,
             _ => return Result::Err(Error::CardError(card)),
         };
@@ -233,37 +260,129 @@ mod test {
         assert_eq!(calc_expedition_score(&"d".to_string()).unwrap(), -40);
         assert_eq!(calc_expedition_score(&"dd".to_string()).unwrap(), -60);
         assert_eq!(calc_expedition_score(&"ddd".to_string()).unwrap(), -80);
-        assert_eq!(calc_expedition_score(&"2345678".to_string()).unwrap(), 2 + 3 + 4 + 5 + 6 + 7 + 8 - 20);
-        assert_eq!(calc_expedition_score(&"23456789".to_string()).unwrap(), 44 - 20 + 20);
-        assert_eq!(calc_expedition_score(&"23456789t".to_string()).unwrap(), 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 - 20 + 20);
+        assert_eq!(
+            calc_expedition_score(&"2345678".to_string()).unwrap(),
+            2 + 3 + 4 + 5 + 6 + 7 + 8 - 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"23456789".to_string()).unwrap(),
+            44 - 20 + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"23456789t".to_string()).unwrap(),
+            2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 - 20 + 20
+        );
         assert_eq!(calc_expedition_score(&"7891".to_string()).unwrap(), 14);
         assert_eq!(calc_expedition_score(&"d7891".to_string()).unwrap(), 14 * 2);
-        assert_eq!(calc_expedition_score(&"dd7891".to_string()).unwrap(), 14 * 3);
-        assert_eq!(calc_expedition_score(&"dd789t".to_string()).unwrap(), 14 * 3);
+        assert_eq!(
+            calc_expedition_score(&"dd7891".to_string()).unwrap(),
+            14 * 3
+        );
+        assert_eq!(
+            calc_expedition_score(&"dd789t".to_string()).unwrap(),
+            14 * 3
+        );
         assert_eq!(calc_expedition_score(&"2".to_string()).unwrap(), -18);
-        assert_eq!(calc_expedition_score(&"23".to_string()).unwrap(), -20 + 2 + 3);
-        assert_eq!(calc_expedition_score(&"234".to_string()).unwrap(), -20 + 2 + 3 + 4);
-        assert_eq!(calc_expedition_score(&"23456789".to_string()).unwrap(), -20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 20);
-        assert_eq!(calc_expedition_score(&"2345678".to_string()).unwrap(), -20 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
-        assert_eq!(calc_expedition_score(&"d23456789".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 2) + 20);
-        assert_eq!(calc_expedition_score(&"dd23456789".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 3) + 20);
-        assert_eq!(calc_expedition_score(&"ddd23456789".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 4) + 20);
-        assert_eq!(calc_expedition_score(&"ddd2345678".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8) * 4) + 20);
-        assert_eq!(calc_expedition_score(&"ddd23456".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5 + 6) * 4) + 20);
-        assert_eq!(calc_expedition_score(&"ddd2345".to_string()).unwrap(), ((-20 + 2 + 3 + 4 + 5) * 4));
+        assert_eq!(
+            calc_expedition_score(&"23".to_string()).unwrap(),
+            -20 + 2 + 3
+        );
+        assert_eq!(
+            calc_expedition_score(&"234".to_string()).unwrap(),
+            -20 + 2 + 3 + 4
+        );
+        assert_eq!(
+            calc_expedition_score(&"23456789".to_string()).unwrap(),
+            -20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"2345678".to_string()).unwrap(),
+            -20 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+        );
+        assert_eq!(
+            calc_expedition_score(&"d23456789".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 2) + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"dd23456789".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 3) + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"ddd23456789".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) * 4) + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"ddd2345678".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5 + 6 + 7 + 8) * 4) + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"ddd23456".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5 + 6) * 4) + 20
+        );
+        assert_eq!(
+            calc_expedition_score(&"ddd2345".to_string()).unwrap(),
+            ((-20 + 2 + 3 + 4 + 5) * 4)
+        );
     }
 
     #[test]
     fn test_calc_player_round_score() {
-        assert_eq!(calc_player_round_score(&"28t 28t".to_string()).unwrap().result, 0);
-        assert_eq!(calc_player_round_score(&"d d d d d".to_string()).unwrap().result, -200);
-        assert_eq!(calc_player_round_score(&"dd dd dd dd dd".to_string()).unwrap().result, -300);
-        assert_eq!(calc_player_round_score(&"ddd d ddd d ddd".to_string()).unwrap().result, -320);
-        assert_eq!(calc_player_round_score(&"2 d34 dd456 ddd5678 ddd23456789t".to_string()).unwrap().result, 121);
-        assert_eq!(calc_player_round_score(&"ddd23456789t".to_string()).unwrap().result, 156);
-        assert_eq!(calc_player_round_score(&"ddd23456789t ddd23456789t ddd23456789t ddd23456789t ddd23456789t".to_string()).unwrap().result, 780);
-        assert_eq!(calc_player_round_score(&"45789t dd3458t d23478t".to_string()).unwrap().result, 81);
-        assert_eq!(calc_player_round_score(&"d234689 d23569t 69 dd56789t".to_string()).unwrap().result, 144);
+        assert_eq!(
+            calc_player_round_score(&"28t 28t".to_string())
+                .unwrap()
+                .result,
+            0
+        );
+        assert_eq!(
+            calc_player_round_score(&"d d d d d".to_string())
+                .unwrap()
+                .result,
+            -200
+        );
+        assert_eq!(
+            calc_player_round_score(&"dd dd dd dd dd".to_string())
+                .unwrap()
+                .result,
+            -300
+        );
+        assert_eq!(
+            calc_player_round_score(&"ddd d ddd d ddd".to_string())
+                .unwrap()
+                .result,
+            -320
+        );
+        assert_eq!(
+            calc_player_round_score(&"2 d34 dd456 ddd5678 ddd23456789t".to_string())
+                .unwrap()
+                .result,
+            121
+        );
+        assert_eq!(
+            calc_player_round_score(&"ddd23456789t".to_string())
+                .unwrap()
+                .result,
+            156
+        );
+        assert_eq!(
+            calc_player_round_score(
+                &"ddd23456789t ddd23456789t ddd23456789t ddd23456789t ddd23456789t".to_string()
+            )
+            .unwrap()
+            .result,
+            780
+        );
+        assert_eq!(
+            calc_player_round_score(&"45789t dd3458t d23478t".to_string())
+                .unwrap()
+                .result,
+            81
+        );
+        assert_eq!(
+            calc_player_round_score(&"d234689 d23569t 69 dd56789t".to_string())
+                .unwrap()
+                .result,
+            144
+        );
     }
 
     #[test]
@@ -274,11 +393,23 @@ mod test {
         assert_eq!(sanity_check_player_cards(&"2 2 2 2s"), false);
         assert_eq!(sanity_check_player_cards(&"2 2 2 2"), true);
         assert_eq!(sanity_check_player_cards(&""), false);
-        assert_eq!(sanity_check_player_cards(&"d23 dd345 ddd45678 ddd 23456y"), false);
+        assert_eq!(
+            sanity_check_player_cards(&"d23 dd345 ddd45678 ddd 23456y"),
+            false
+        );
         assert_eq!(sanity_check_player_cards(&"dddd"), false);
         assert_eq!(sanity_check_player_cards(&"dd23456789t dd23456789t"), true);
-        assert_eq!(sanity_check_player_cards(&"t98765432ddd t98765432ddd"), true);
-        assert_eq!(sanity_check_player_cards(&"t98765432dddt98765432ddd"), false);
-        assert_eq!(sanity_check_player_cards(&"t98765432ddd t987654932ddd"), false);
+        assert_eq!(
+            sanity_check_player_cards(&"t98765432ddd t98765432ddd"),
+            true
+        );
+        assert_eq!(
+            sanity_check_player_cards(&"t98765432dddt98765432ddd"),
+            false
+        );
+        assert_eq!(
+            sanity_check_player_cards(&"t98765432ddd t987654932ddd"),
+            false
+        );
     }
 }
